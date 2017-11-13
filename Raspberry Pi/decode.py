7412a921fecabc928059a6ff6bf91296c6f089bb
Austin Patel <austinapatel@gmail.com>
2017-11-13 12:44:18 -0800

    Tried to fix decode

--- Raspberry Pi/decode.py
+++ Raspberry Pi/decode.py
@@ -1,3 +1,9 @@
-fh = open("imageToSave.png", "wb")
-fh.write(str.decode('base64'))
-fh.close()
+content = open('img_data.txt','r').read()
+
+# fh = open("imageToSave.png", "wb")
+# fh.write(content.decode('base64'))
+# fh.close()
+import base64
+with open("imageToSave.png", "wb") as fh:
+    fh.write(base64.standard_b64decode(content))
+    fh.close()
